539d490f8b2f5bd6cededcdeebc747ed62d05ccd
rafaelsilva1406 <rafaelsilva1406@gmail.com>
2025-06-17 21:52:55 -0700

    Init nextjs, react, bootstrap

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+       <h1>Hello World!</h1>
+      </main>
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  );
+}
